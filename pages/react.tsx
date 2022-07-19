33c8cb656bc6e2d3e730e29490e16f4ba3f7ab90
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-07-19 14:22:09 -0700

    added React page

---
+++ pages/react.tsx
@@ -0,0 +1,31 @@
+import { nextseo } from "next-seo";
+import Content from "../components/Content";
+import PageTitle from "../components/PageTitle";
+import Link from "../components/Link";
+import Image from "../components/Image";
+import Blockquote from "../components/Blockquote";
+import CodeBlock from "../components/CodeBlock";
+import { H2 } from "../components/Heading";
+import { UnorderedList, ListItem } from "../components/List";
+import { useState } from "react";
+
+const React = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      <H2>basic useState hooks example</H2>
+      Whatever gets input here :
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+          console.log("s");
+        }}
+      />
+      <br />
+      Should also show up here: <span>{name}</span>
+    </>
+  );
+};
+
+export default React;
