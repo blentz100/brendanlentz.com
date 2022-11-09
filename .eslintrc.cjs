938d3e89377a9030f57e7a48a38391fbd96c12d1
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-11-09 14:02:37 -0700

    reverted printwidth to original setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ module.exports = {
         // normal .prettierrc config:
         singleQuote: false,
         tabWidth: 2,
-        printWidth: 80,
+        printWidth: 120,
       },
     ],
   },
@@ -38,7 +38,7 @@ module.exports = {
             // normal .prettierrc config:
             singleQuote: false,
             tabWidth: 2,
-            printWidth: 80,
+            printWidth: 120,
           },
         ],
       },
