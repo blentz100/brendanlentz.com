4427195daed1c880bb90fe915573f582ef9b5bf7
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-11-09 15:20:41 -0700

    fixed linting errors

--- pages/formik.tsx
+++ pages/formik.tsx
@@ -1,55 +1,48 @@
-import * as React from 'react';
- import {
-   Formik,
-   FormikHelpers,
-   FormikProps,
-   Form,
-   Field,
-   FieldProps,
- } from 'formik';
-import { values } from 'faunadb';
- 
- interface MyFormValues {
-   fahrenheit: string;
- }
- 
- export const MyApp: React.FC<{}> = () => {
-   const initialValues: MyFormValues = { fahrenheit: '' };
-   return (
-     <div>
-       <h1>My Example</h1>
-       <Formik
-         initialValues={initialValues}
-         onSubmit={(values, actions) => {
-           console.log({ values, actions });
-           actions.setSubmitting(false);
-         }}
-       >
-         <Form>
-           <label htmlFor="fahrenheit">Fahrenheit: </label>
-           <Field id="fahrenheit" name="fahrenheit" placeholder="Fahrenheit" />
-           <button type="submit">Convert</button>
-           <div>Celsius: {initialValues.fahrenheit} </div>
-         </Form>
-       </Formik>
-        <DisplayConversion {...initialValues}/>
-     </div>
-   );
- };
+import * as React from "react";
+import { Formik, Form, Field } from "formik";
+import { values } from "faunadb";
 
-export const DisplayConversion: React.FC<{}> = props =>
-  <div style={{ margin: '1rem 0' }}>
-    <h3 style={{ fontFamily: 'monospace' }} />
+interface MyFormValues {
+  fahrenheit: string;
+}
+
+export const MyApp: React.FC<unknown> = () => {
+  const initialValues: MyFormValues = { fahrenheit: "" };
+  return (
+    <div>
+      <h1>My Example</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions });
+          actions.setSubmitting(false);
+        }}
+      >
+        <Form>
+          <label htmlFor="fahrenheit">Fahrenheit: </label>
+          <Field id="fahrenheit" name="fahrenheit" placeholder="Fahrenheit" />
+          <button type="submit">Convert</button>
+          <div>Celsius: {initialValues.fahrenheit} </div>
+        </Form>
+      </Formik>
+      <DisplayConversion {...initialValues} />
+    </div>
+  );
+};
+
+export const DisplayConversion: React.FC<unknown> = (props) => (
+  <div style={{ margin: "1rem 0" }}>
+    <h3 style={{ fontFamily: "monospace" }} />
     <pre
       style={{
-        background: '#f6f8fa',
-        fontSize: '.65rem',
-        padding: '.5rem',
+        background: "#f6f8fa",
+        fontSize: ".65rem",
+        padding: ".5rem",
       }}
     >
-      <strong>props</strong> ={' '}
-      {JSON.stringify(props, null, 2)}
+      <strong>props</strong> = {JSON.stringify(props, null, 2)}
     </pre>
-  </div>;
+  </div>
+);
 
 export default MyApp;
