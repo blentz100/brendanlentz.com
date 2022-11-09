4e7aa633560707ffda6cb64f7314c13aebf8475b
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-11-09 15:34:10 -0700

    cleared error

--- pages/formik.tsx
+++ pages/formik.tsx
@@ -25,7 +25,6 @@ export const MyApp: React.FC<unknown> = () => {
           <div>Celsius: {initialValues.fahrenheit} </div>
         </Form>
       </Formik>
-      <DisplayConversion {...initialValues} />
     </div>
   );
 };
