a673a1e4759779686a710e39d00b7aa93e2fdc2a
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2023-06-26 15:28:40 -0700

    Add DisplayFormikStateComponent

--- pages/formik.tsx
+++ pages/formik.tsx
@@ -6,10 +6,27 @@ interface MyFormValues {
   fahrenheit: string;
 }
 
+const DisplayFormikState = (props: any) => {
+  return (
+    <div style={{ margin: "1rem 0" }}>
+      <h3 style={{ fontFamily: "monospace" }} />
+      <pre
+        style={{
+          background: "#f6f8fa",
+          fontSize: ".65rem",
+          padding: ".5rem",
+        }}
+      >
+        <strong>props</strong> = {JSON.stringify(props, null, 2)}
+      </pre>
+    </div>
+  );
+};
+
 export const MyApp: React.FC<unknown> = () => {
   const initialValues: MyFormValues = { fahrenheit: "" };
   return (
-    <div>
+    <>
       <h1>My Example</h1>
       <Formik
         initialValues={initialValues}
@@ -18,14 +35,18 @@ export const MyApp: React.FC<unknown> = () => {
           actions.setSubmitting(false);
         }}
       >
-        <Form>
-          <label htmlFor="fahrenheit">Fahrenheit: </label>
-          <Field id="fahrenheit" name="fahrenheit" placeholder="Fahrenheit" />
-          <button type="submit">Convert</button>
-          <div>Celsius: {initialValues.fahrenheit} </div>
-        </Form>
+        {({ values }) => (
+          <Form>
+            <label htmlFor="fahrenheit">Fahrenheit: </label>
+            <Field id="fahrenheit" name="fahrenheit" placeholder="Fahrenheit" />
+            <button type="submit">Convert</button>
+            <div>Celsius: {initialValues.fahrenheit} </div>
+            <DisplayFormikState props={values} />
+          </Form>
+        )}
       </Formik>
-    </div>
+      ;
+    </>
   );
 };
 
