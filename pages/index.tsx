8c65ce3150836b30428e59702eea58af89037c74
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2024-01-07 15:27:44 -0700

    Revert "Moved Habit Tracker code into the components folder."
    This reverts commit 9e4dacf1d5d63a20ff848a112efe32b0f2727caa.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Link, { LinkProps } from "../components/Link";
 import { styled, keyframes, darkTheme } from "../lib/styles/stitches.config";
+import { sortBy } from "lodash";
+import { useEffect, useState } from "react";
+import { Badge, Container } from "@mantine/core";
+import { DataTable, DataTableSortStatus } from "mantine-datatable";
+import { returnTotal, returnTotalPercentage } from "../lib/helpers/data-table";
+import { TableFooter } from "../components/TableFooter/TableFooter";
 import { Temporal } from "@js-temporal/polyfill";
-import { HabitTrackerTable } from "../components/DataTable";
 
 export interface RecordType {
   date: string;
@@ -124,6 +129,9 @@ const EasterEgg = styled(ColorfulLink, {
 });
 
 interface IndexProps {
+  staticRecords: RecordType[];
+}
+interface HabitTrackerProps {
   staticRecords: RecordType[];
 }
 
@@ -252,10 +260,7 @@ const Index = ({ staticRecords }: IndexProps) => {
         </ColorfulLink>
         .
       </Paragraph>
-      <H2>Habit Tracker</H2>
-      <Paragraph>
-        I created a habit tracker to track my fitness goal and some other exercises. Updated in realtime.
-      </Paragraph>
+
       <HabitTrackerTable staticRecords={staticRecords} />
     </>
   );
@@ -276,6 +281,122 @@ export async function getStaticProps() {
     },
     revalidate: 300,
   };
+}
+export function HabitTrackerTable({ staticRecords }: HabitTrackerProps) {
+  const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+    columnAccessor: "dateAsNumber",
+    direction: "desc",
+  });
+  const [records, setRecords] = useState<RecordType[]>(staticRecords);
+
+  // sort functionality
+  useEffect(() => {
+    const data = sortBy(records, sortStatus.columnAccessor);
+    setRecords(sortStatus.direction === "desc" ? data.reverse() : data);
+  }, [sortStatus]);
+
+  return (
+    <article>
+      <H2>Habit Tracker</H2>
+      <Paragraph>
+        I created a habit tracker to track my fitness goal and some other exercises. Updated in realtime.
+      </Paragraph>
+      <Container mt={20} ml={-15}>
+        <DataTable
+          columns={[
+            {
+              accessor: "dateAsNumber",
+              title: "Date",
+              sortable: true,
+              footer: <>Progress</>,
+              render: (item) => (
+                <div>
+                  {item.date}
+                  {item.date === new Date().toLocaleDateString() ? <Badge color="blue"> Today </Badge> : null}
+                </div>
+              ),
+            },
+            {
+              accessor: "pushups",
+              sortable: true,
+              textAlignment: "center",
+              footer: (
+                <TableFooter
+                  total={returnTotal(records, "pushups")}
+                  habit="pushups"
+                  goal={10000}
+                  totalPercentage={returnTotalPercentage(records, "pushups", 10000)}
+                />
+              ),
+            },
+            {
+              accessor: "situps",
+              sortable: true,
+              textAlignment: "center",
+              footer: (
+                <TableFooter
+                  total={returnTotal(records, "situps")}
+                  habit="situps"
+                  goal={3000}
+                  totalPercentage={returnTotalPercentage(records, "situps", 3000)}
+                />
+              ),
+            },
+            {
+              accessor: "jacks",
+              sortable: true,
+              textAlignment: "center",
+              title: "Jumping Jacks",
+              footer: (
+                <TableFooter
+                  total={returnTotal(records, "jacks")}
+                  habit="jacks"
+                  goal={6000}
+                  totalPercentage={returnTotalPercentage(records, "jacks", 6000)}
+                />
+              ),
+            },
+            {
+              accessor: "stairs",
+              sortable: true,
+              textAlignment: "center",
+              footer: (
+                <TableFooter
+                  total={returnTotal(records, "stairs")}
+                  habit="stairs"
+                  goal={200}
+                  totalPercentage={returnTotalPercentage(records, "stairs", 200)}
+                />
+              ),
+            },
+            {
+              accessor: "pullups",
+              sortable: true,
+              textAlignment: "center",
+              footer: (
+                <TableFooter
+                  total={returnTotal(records, "pullups")}
+                  habit="pullups"
+                  goal={400}
+                  totalPercentage={returnTotalPercentage(records, "pullups", 400)}
+                />
+              ),
+            },
+          ]}
+          records={records}
+          withBorder
+          striped
+          height={500}
+          highlightOnHover
+          scrollAreaProps={{ type: "always" }}
+          sortStatus={sortStatus}
+          onSortStatusChange={setSortStatus}
+          idAccessor={"date"}
+        />
+        {/* <figcaption><i>updated: {lastEditedDate}</i></figcaption> */}
+      </Container>
+    </article>
+  );
 }
 
 export default Index;
