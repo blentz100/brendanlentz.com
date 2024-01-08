aa42f4597fa2d8ea8215ef20bb34e9dd26718377
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2024-01-07 18:55:31 -0700

    Troubleshooting build error

--- pages/api/sheets.ts
+++ pages/api/sheets.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   });
 
   const sheets = google.sheets({ version: "v4", auth });
-  const range = `Habits!A8:G372`; // the habit data
+  const range = `2024!A8:G372`; // the habit data
 
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
