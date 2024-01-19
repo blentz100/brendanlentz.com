61d0094af0cda7efb69c5aef4dd001d6aab5f2b0
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2024-01-19 10:54:12 -0700

    Fix TypeScript Warnings related to accessing object properties dynamically.
    Reference: https://stackoverflow.com/questions/62438346/how-to-dynamically-access-object-property-in-typescript

--- lib/helpers/data-table.ts
+++ lib/helpers/data-table.ts
@@ -1,14 +1,20 @@
-export function returnTotal(records: any[] | undefined, habit: string) {
+import { RecordType } from "../../pages";
+
+// Solved TypeScript issues using this as a guide
+// https://stackoverflow.com/questions/62438346/how-to-dynamically-access-object-property-in-typescript
+// You can't just use the habit variable to index the currentValue object.
+// TypeScript doesn't allow that
+export function returnTotal(records: RecordType[], habit: string) {
   return records?.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue[habit];
+    return accumulator + (currentValue[habit as keyof typeof currentValue] as number);
   }, 0);
 }
 
-export function returnTotalPercentage(records: any[] | undefined, habit: string, goal: number) {
+export function returnTotalPercentage(records: RecordType[], habit: string, goal: number) {
   return Math.min(
     Math.floor(
       (records?.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue[habit];
+        return accumulator + (currentValue[habit as keyof typeof currentValue] as number);
       }, 0) /
         goal) *
         100
