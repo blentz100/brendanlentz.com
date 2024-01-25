7485a9aa480a9597ca69b59d64434b4d0892741d
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2024-01-24 21:54:22 -0700

    Troubleshooting build error

--- components/TableFooter/TableFooter.tsx
+++ components/TableFooter/TableFooter.tsx
@@ -22,53 +22,53 @@ export const TableFooter: React.FC<Props> = ({
       <div>TableFooter</div>
     </>
   );
-  return (
-    <>
-      <Table>
-        <tbody>
-          <tr key={0}>
-            <td>
-              <Text align={"left"}>{topLabel}</Text>
-            </td>
-            <td>
-              <Text align={"right"}>{Intl.NumberFormat().format(total)}</Text>
-            </td>
-          </tr>
-          <tr key={1}>
-            <td>
-              <Text align={"left"}>{bottomLabel}</Text>
-            </td>
-            <td>
-              <Text align={"right"}>{Intl.NumberFormat().format(goal)}</Text>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <Center>
-        <RingProgress
-          thickness={7}
-          size={80}
-          sections={[
-            {
-              value: totalPercentage,
-              color: "blue",
-              tooltip: `${total} ${habit} completed`,
-            },
-            {
-              value: totalPercentage > 100 ? 0 : 100 - totalPercentage,
-              color: "lightgrey",
-              tooltip: `${goal - total} ${habit} to go`,
-            },
-          ]}
-          label={
-            <Center>
-              <Text size="m" fw={700} ta="center">
-                {totalPercentage}%
-              </Text>
-            </Center>
-          }
-        ></RingProgress>
-      </Center>
-    </>
-  );
+  // return (
+  //   <>
+  //     <Table>
+  //       <tbody>
+  //         <tr key={0}>
+  //           <td>
+  //             <Text align={"left"}>{topLabel}</Text>
+  //           </td>
+  //           <td>
+  //             <Text align={"right"}>{Intl.NumberFormat().format(total)}</Text>
+  //           </td>
+  //         </tr>
+  //         <tr key={1}>
+  //           <td>
+  //             <Text align={"left"}>{bottomLabel}</Text>
+  //           </td>
+  //           <td>
+  //             <Text align={"right"}>{Intl.NumberFormat().format(goal)}</Text>
+  //           </td>
+  //         </tr>
+  //       </tbody>
+  //     </Table>
+  //     <Center>
+  //       <RingProgress
+  //         thickness={7}
+  //         size={80}
+  //         sections={[
+  //           {
+  //             value: totalPercentage,
+  //             color: "blue",
+  //             tooltip: `${total} ${habit} completed`,
+  //           },
+  //           {
+  //             value: totalPercentage > 100 ? 0 : 100 - totalPercentage,
+  //             color: "lightgrey",
+  //             tooltip: `${goal - total} ${habit} to go`,
+  //           },
+  //         ]}
+  //         label={
+  //           <Center>
+  //             <Text size="m" fw={700} ta="center">
+  //               {totalPercentage}%
+  //             </Text>
+  //           </Center>
+  //         }
+  //       ></RingProgress>
+  //     </Center>
+  //   </>
+  // );
 };
