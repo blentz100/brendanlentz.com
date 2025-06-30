feae2593876b0c3b139a078f21cf8799cd440171
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-06-30 13:09:21 -0700

    HT-30 Add Logging
    Add logging to troubleshoot Vercel deployment issue.

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+
+console.log("*Env:", {
+  url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10),
+});
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log('supabase: ', supabase);
+console.log("supabase: ", supabase);
